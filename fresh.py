9f12a23309a74c13c265e2a1281924db6d85efcb
stephen911 <stephendappah1@gmail.com>
2022-03-31 00:19:22 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -94,4 +94,5 @@ for i in df.index:
                                                                                                     
                                                                                                     
                                                                                                         ~ print(stopwords.words('english'))
-                                                                                                        
+                                                                                                            stop_words = set(stopwords.words('english'))
+                                                                                                            
