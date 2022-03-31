b9e568ad816d311076a8e9d3f3cc56612f9035fc
stephen911 <stephendappah1@gmail.com>
2022-03-31 00:15:18 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -61,4 +61,5 @@ for i in df.index:
                 ~ Defining vec_space_method
                 def vec_space_method(recipe_query):
                         document = df["combine_features"]
-                        
+                            ~ print(document)
+                            
