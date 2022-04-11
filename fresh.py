7dd238697e343796a2225f63972aed3a214e2e22
stephen911 <stephendappah1@gmail.com>
2022-04-11 16:41:31 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -90,3 +90,4 @@ for i in df.index:
                                                                                             ~ print(df1)
                                                                                             
                                                                                             
+                                                                                            
