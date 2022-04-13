05a7f10a9e7ad2efa8aefe57d49419b11a27fa9c
stephen911 <stephendappah1@gmail.com>
2022-04-13 13:35:06 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -2,3 +2,5 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
